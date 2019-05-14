f2a17a150afb15f4fbd6e04609101f005e76b244
yzyray <348533481@qq.com>
2019-05-15 00:19:54 +0800

    保存

--- src/main/java/featurecat/lizzie/Lizzie.java
+++ src/main/java/featurecat/lizzie/Lizzie.java
@@ -148,7 +148,7 @@ public class Lizzie {
       }
     }
 
-    Lizzie.leelaz.sendCommand("name");
+    //  Lizzie.leelaz.sendCommand("name");
 
     int movenumber = board.getcurrentmovenumber();
     board.saveMoveNumber();
