3fbfb22bbead8e7dc9a4997dc478698417d1e580
yzyray <348533481@qq.com>
2019-05-30 20:15:32 +0800

    X快捷键提示有待重做

--- src/main/java/featurecat/lizzie/gui/Input.java
+++ src/main/java/featurecat/lizzie/gui/Input.java
@@ -420,18 +420,20 @@ public class Input implements MouseListener, KeyListener, MouseWheelListener, Mo
       case VK_X:
         if (controlIsPressed(e)) {
           Lizzie.frame.openConfigDialog();
-        } else {
-          if (!Lizzie.frame.showControls) {
-            if (Lizzie.leelaz.isPondering()) {
-              wasPonderingWhenControlsShown = true;
-              // Lizzie.leelaz.togglePonder();
-            } else {
-              wasPonderingWhenControlsShown = false;
-            }
-            Lizzie.frame.drawControls();
-          }
-          Lizzie.frame.showControls = true;
         }
+        //        else {
+        //          if (!Lizzie.frame.showControls) {
+        //            if (Lizzie.leelaz.isPondering()) {
+        //              wasPonderingWhenControlsShown = true;
+        //               Lizzie.leelaz.togglePonder();
+        //            } else {
+        //              wasPonderingWhenControlsShown = false;
+        //            }
+        //
+        //            Lizzie.frame.drawControls();
+        //          }
+        //          Lizzie.frame.showControls = true;
+        //        }
         break;
 
       case VK_W:
@@ -595,11 +597,11 @@ public class Input implements MouseListener, KeyListener, MouseWheelListener, Mo
   @Override
   public void keyReleased(KeyEvent e) {
     switch (e.getKeyCode()) {
-      case VK_X:
-        if (wasPonderingWhenControlsShown) // Lizzie.leelaz.togglePonder();
-        Lizzie.frame.showControls = false;
-        Lizzie.frame.repaint();
-        break;
+        //      case VK_X:
+        //        if (wasPonderingWhenControlsShown)  Lizzie.leelaz.togglePonder();
+        //        Lizzie.frame.showControls = false;
+        //        Lizzie.frame.repaint();
+        //        break;
 
       case VK_Z:
         stopTemporaryBoard();
