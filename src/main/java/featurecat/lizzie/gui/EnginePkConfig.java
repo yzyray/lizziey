d15c4a81194681b7cf240599ca11d89a727f2d79
yzyray <348533481@qq.com>
2019-06-18 20:33:14 +0800

    保存

--- src/main/java/featurecat/lizzie/gui/EnginePkConfig.java
+++ src/main/java/featurecat/lizzie/gui/EnginePkConfig.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -30,6 +32,7 @@ public class EnginePkConfig extends JDialog {
     setType(Type.POPUP);
     setTitle("引擎对战设置");
     setBounds(0, 0, 480, 300);
+    setResizable(false);
     setAlwaysOnTop(Lizzie.frame.isAlwaysOnTop());
     setLayout(null);
     setLocationRelativeTo(getOwner());
@@ -66,6 +69,23 @@ public class EnginePkConfig extends JDialog {
     rdoGenmove = new JRadioButton("genmove模式对战");
     rdoAna = new JRadioButton("分析模式对战");
 
+    rdoGenmove.addFocusListener(
+        new FocusListener() {
+
+          @Override
+          public void focusGained(FocusEvent arg0) {
+            // TODO Auto-generated method stub
+            txtresignSetting.setEnabled(false);
+            txtresignSetting2.setEnabled(false);
+          }
+
+          @Override
+          public void focusLost(FocusEvent arg0) {
+            // TODO Auto-generated method stub
+            txtresignSetting.setEnabled(true);
+            txtresignSetting2.setEnabled(true);
+          }
+        });
     ButtonGroup wrgroup = new ButtonGroup();
     wrgroup.add(rdoGenmove);
     wrgroup.add(rdoAna);
