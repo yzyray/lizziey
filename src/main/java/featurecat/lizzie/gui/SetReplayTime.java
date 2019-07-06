8ccb5c807a3853e830b7fa105d692de09218433b
yzyray <348533481@qq.com>
2019-07-07 00:54:17 +0800

    1

---
+++ src/main/java/featurecat/lizzie/gui/SetReplayTime.java
@@ -0,0 +1,109 @@
+package featurecat.lizzie.gui;
+
+import featurecat.lizzie.Lizzie;
+import java.awt.BorderLayout;
+import java.awt.Window.Type;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.ResourceBundle;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.DocumentFilter.FilterBypass;
+import javax.swing.text.InternationalFormatter;
+
+public class SetReplayTime extends JDialog {
+  private JFormattedTextField time;
+  public final ResourceBundle resourceBundle = ResourceBundle.getBundle("l10n.DisplayStrings");
+  private int replaytime;
+  private static JTextField defaultText = new JTextField();
+
+  public SetReplayTime() {
+    setType(Type.POPUP);
+    setTitle("设置分支回放间隔");
+    setAlwaysOnTop(Lizzie.frame.isAlwaysOnTop());
+    setBounds(0, 0, 340, 130);
+    getContentPane().setLayout(new BorderLayout());
+    JPanel buttonPane = new JPanel();
+    getContentPane().add(buttonPane, BorderLayout.CENTER);
+    JButton okButton = new JButton("确定");
+    okButton.setBounds(120, 55, 74, 29);
+
+    okButton.addActionListener(
+        new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+
+            setVisible(false);
+            applyChange();
+          }
+        });
+    buttonPane.setLayout(null);
+    okButton.setActionCommand("OK");
+    buttonPane.add(okButton);
+    getRootPane().setDefaultButton(okButton);
+    NumberFormat nf = NumberFormat.getIntegerInstance();
+    nf.setGroupingUsed(false);
+
+    JLabel lblChangeTo = new JLabel("设置分支回访间隔(毫秒)：");
+    lblChangeTo.setBounds(45, 24, 150, 20);
+    buttonPane.add(lblChangeTo);
+    lblChangeTo.setHorizontalAlignment(SwingConstants.LEFT);
+
+    time =
+        new JFormattedTextField(
+            new InternationalFormatter(nf) {
+              protected DocumentFilter getDocumentFilter() {
+                return filter;
+              }
+
+              private DocumentFilter filter = new DigitOnlyFilter();
+            });
+    time.setBounds(190, 26, 30, 19);
+    buttonPane.add(time);
+    time.setColumns(3);
+
+    time.setText((int) (Lizzie.config.replayBranchIntervalSeconds * 1000) + "");
+
+    setLocationRelativeTo(getOwner());
+  }
+
+  private class DigitOnlyFilter extends DocumentFilter {
+    @Override
+    public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr)
+        throws BadLocationException {
+      String newStr = string != null ? string.replaceAll("\\D++", "") : "";
+      if (!newStr.isEmpty()) {
+        fb.insertString(offset, newStr, attr);
+      }
+    }
+  }
+
+  private void applyChange() {
+    replaytime = txtFieldValue(time);
+    Lizzie.config.replayBranchIntervalSeconds = replaytime / 1000.0;
+    Lizzie.config.uiConfig.put("replay-branch-interval-seconds", replaytime / 1000.0);
+    try {
+      Lizzie.config.save();
+    } catch (IOException es) {
+      // TODO Auto-generated catch block
+    }
+  }
+
+  private Integer txtFieldValue(JTextField txt) {
+    if (txt.getText().trim().isEmpty()
+        || txt.getText().trim().length() >= String.valueOf(Integer.MAX_VALUE).length()) {
+      return 0;
+    } else {
+      return Integer.parseInt(txt.getText().trim());
+    }
+  }
+}
